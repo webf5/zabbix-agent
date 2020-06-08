afcf6b6b57f8e4eb9664a6c0aa9f8a216346ed25
webf5 <591454723@qq.com>
2020-06-08 16:06:26 +0800

    first commit

---
+++ docker_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#安装zabbix-agent自动化脚本
+
+#首先需要修改docker-compose文件的环境参数
+
+#可修改参数：
+#ZBX_SERVER_HOST=Zabbix-Server服务器ip
+#ports:本机端口:10050
+
+docker-compose -f -d zabbix-agent.yml up 
+
+
+
